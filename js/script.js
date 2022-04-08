4883cd1478d3bc5ea576e80ab6a3741b11ee7e8f
esempe <steam.dota.return@gmail.com>
2022-04-08 15:05:37 +0300

    added function round, that contains game logic

--- js/script.js
+++ js/script.js
@@ -13,5 +13,32 @@ computerPlay = function () {
     }
     return computerMoveString
 }
-console.log(computerPlay());
+
+round = function (playerSelection, computerSelection) {
+    computerSelection = computerPlay();
+    playerSelection = prompt('Pick your weapon;', 'Rock/Paper/Scissors');
+
+    if (computerSelection === 'Rock' && playerSelection == 'Paper' ){
+        roundResult = 'You win, Paper beats Rock';
+    } else if (computerSelection === 'Paper' && playerSelection == 'Paper' ){
+        roundResult = 'Draw';
+    } else if (computerSelection === 'Scissors' && playerSelection == 'Paper' ){
+        roundResult = 'You Lose, Scissors beats Paper'
+    } else if (computerSelection === 'Scissors' && playerSelection == 'Scissors' ){
+        roundResult = 'Draw';
+    }  else if (computerSelection === 'Paper' && playerSelection == 'Scissors' ){
+        roundResult = 'You win, Scissors beats Paper'
+    } else if (computerSelection === 'Rock' && playerSelection == 'Scissors' ){
+        roundResult = 'You lose, Rock beats Scissors';
+    } else if (computerSelection === 'Rock' && playerSelection == 'Rock' ){
+        roundResult = 'Draw';
+    } else if (computerSelection === 'Paper' && playerSelection == 'Rock' ){
+        roundResult = 'Draw';
+    } else if (computerSelection === 'Scissors' && playerSelection == 'Rock' ){
+        roundResult = 'Draw';
+    }
+    return roundResult;
+}
+
+console.log(round())
 
