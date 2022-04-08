53d4a4b2b7f256445713d184109b3d71f5aa27da
esempe <steam.dota.return@gmail.com>
2022-04-08 13:50:49 +0300

    added computerPlay function, that randomly choose paper,rock, Scissors

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log(6);
+
+
+
+computerPlay = function () {
+
+    computerMoveInt = Math.floor(Math.random() * 3);
+    if (computerMoveInt === 0) {
+        computerMoveString = 'Rock';
+    } else if (computerMoveInt === 1) {
+        computerMoveString = 'Paper';
+    } else {
+        computerMoveString = 'Scissors';
+    }
+    return computerMoveString
+}
+console.log(computerPlay());
+
